0069bd85b96e92b666518d972e0b2b2e91036b21
victorversus <45893619+victorversus@users.noreply.github.com>
2018-12-15 12:55:07 +0400

    add initila website files

---
+++ scriptfile.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+var msg = function () {
+  let myUser = document.getElementById("myuser").value;
+  let myPass = document.getElementById("mypass").value;
+if(myUser=="vusal" && myPass=="12345") alert(`hello ${myUser}`);
+else if(myUser != "vusal") {
+var myElement = document.getElementsByClassName("incorrect")
+myElement.innerHTML = "salam"
+}
+	
+	
+	
+	
+}
